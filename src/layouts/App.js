bdd962fc7bcad78ddae85ad1a9c3f1e4e9c291c6
Damian Ludef <damiinho@gmail.com>
2023-03-25 15:25:29 +0100

    first commit, app with react-router-dom

---
+++ src/layouts/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Witam</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
