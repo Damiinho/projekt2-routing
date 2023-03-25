23c8ad4c85d1ce32493a8560021291aa7bf045dc
Damian Ludef <damiinho@gmail.com>
2023-03-25 20:34:10 +0100

    footer fun

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,6 +1,58 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <h2>stopka</h2>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <p>
+            Jesteś na <span>stronie głównej</span>
+          </p>
+        )}
+      />
+      <Route
+        path="/:page"
+        exact
+        render={(props) => (
+          <>
+            <p>
+              Jesteś na <span>stronie {props.match.params.page}</span>
+            </p>
+            <p>
+              Jesteś na <span>stronie {props.match.url}</span>
+            </p>
+            <p>
+              {" "}
+              Jesteś na <span>stronie {props.match.path}</span>
+            </p>
+          </>
+        )}
+      />
+      <Route
+        path="/:page/:idProduct"
+        exact
+        render={(props) => (
+          <>
+            <p>
+              Jesteś na <span>stronie {props.match.params.idProduct}</span>
+            </p>
+            <p>
+              Jesteś na <span>stronie {props.match.url}</span>
+            </p>
+            <p>
+              {" "}
+              Jesteś na <span>stronie {props.match.path}</span>
+            </p>
+          </>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Footer;
