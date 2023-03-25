16588aeb68b880a4aa50b3d8c5327267bbbcfe3c
Damian Ludef <damiinho@gmail.com>
2023-03-25 15:50:34 +0100

    site template

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from "./Footer";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>Witam</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/layouts/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>elo</div>;
+};
+
+export default Header;
