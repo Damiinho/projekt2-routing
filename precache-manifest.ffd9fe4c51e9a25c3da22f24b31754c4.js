217168d5cd8dabe1b95f59a076630237bc5ed91e
Damian Ludef <damiinho@gmail.com>
2023-03-27 18:16:07 +0200

    Updates

---
+++ precache-manifest.ffd9fe4c51e9a25c3da22f24b31754c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54e9f1baae15329ecdff",
+    "url": "/projekt2-routing/static/css/main.2d4b7b19.chunk.css"
+  },
+  {
+    "revision": "54e9f1baae15329ecdff",
+    "url": "/projekt2-routing/static/js/main.54e9f1ba.chunk.js"
+  },
+  {
+    "revision": "a79b457e73982c99b212",
+    "url": "/projekt2-routing/static/js/runtime~main.a79b457e.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/projekt2-routing/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/projekt2-routing/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/projekt2-routing/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/projekt2-routing/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "f12ffe32c1fdfeb5d965f5fbb1abb381",
+    "url": "/projekt2-routing/index.html"
+  }
+];
